0a3e577c0bcf640c2f1b59aaa5ebad8f567c77ea
Vignesh Iyer <1792167@tcs.com>
2021-12-24 15:46:48 +0530

    added maps to webapp

--- BusTrackerWebApp/webApp/src/app/app.component.ts
+++ BusTrackerWebApp/webApp/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare const L: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webApp';
+export class AppComponent  implements OnInit{
+  title = 'BusTrackerApp';
+
+  ngOnInit() {
+    let map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiYnJheDI1MDciLCJhIjoiY2t4azgyOXBxNmRwaDJ1cTNjMGRqcjF3ZCJ9.0pJi--39nsT8km17AeiY3g'
+    }).addTo(map);
+    let marker = L.marker([51.5, -0.09]).addTo(map);
+  }
 }
