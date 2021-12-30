a8f4362465f5361d412cbf717fe11e59b7ea8b3c
Vignesh Iyer <1792167@tcs.com>
2021-12-30 16:32:09 +0530

    new updates to sample api

--- SampleAPI/app.py
+++ SampleAPI/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import flask
-
+import time
 
 app = Flask(__name__)
 
@@ -9,11 +9,17 @@ global record
 def sendEvents():
     record=0
     while(1):
+        record+=1
+        time.sleep(3)
         yield 'data: \t recordNum'+ str(record) +' hello from api!'
 
 @app.route('/messages', methods=['GET'])
 def stream():
     return flask.Response(sendEvents(), mimetype='text/event-stream')
+
+@app.route('/', methods=['GET'])
+def index():
+    return '<H3>Please hit /messages path to get message streams.</H3>'
 
 if __name__ == '__main__':
     app.debug = True
