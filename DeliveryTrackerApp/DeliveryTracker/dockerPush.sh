bba0e87c4da94d63d2132fb8984dcd4ae0457e13
Vignesh Iyer <1792167@tcs.com>
2021-12-31 16:56:54 +0530

    optimized code for tracker and simulator

--- DeliveryTrackerApp/DeliveryTracker/dockerPush.sh
+++ DeliveryTrackerApp/DeliveryTracker/dockerPush.sh
@@ -3,5 +3,5 @@
 set -e
 
 cat ~/my_docker_pass.txt | base64 -d | docker login -u brax2507 --password-stdin
-docker build -t brax2507/bus-1-tracker .
-docker push brax2507/bus-1-tracker:latest
+docker build -t brax2507/delivery-tracker .
+docker push brax2507/delivery-tracker:latest
