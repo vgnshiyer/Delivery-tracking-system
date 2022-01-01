dc1318c50da1527693b2b4629e8aa531a5c9b2f8
Vignesh Iyer <1792167@tcs.com>
2022-01-01 16:22:49 +0530

    changed docker image names

--- DeliveryTrackerApp/DeliverySimulator/dockerPush.sh
+++ DeliveryTrackerApp/DeliverySimulator/dockerPush.sh
@@ -3,5 +3,5 @@
 set -e
 
 cat ~/my_docker_pass.txt | base64 -d | docker login -u brax2507 --password-stdin
-docker build -t brax2507/delivery-simulator .
-docker push brax2507/delivery-simulator:latest
+docker build -t brax2507/delivery-simulator:v1 .
+docker push brax2507/delivery-simulator:v1
