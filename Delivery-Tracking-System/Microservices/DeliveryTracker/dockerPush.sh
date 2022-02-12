bd0f0926c756e1c198826a228d93ade700924e47
Vignesh Iyer <1792167@tcs.com>
2022-02-13 00:15:32 +0530

    changed directory structure

--- Delivery-Tracking-System/Microservices/DeliveryTracker/dockerPush.sh
+++ Delivery-Tracking-System/Microservices/DeliveryTracker/dockerPush.sh
@@ -3,5 +3,5 @@
 set -e
 
 cat ~/my_docker_pass.txt | base64 -d | docker login -u brax2507 --password-stdin
-docker build -t brax2507/delivery-tracker-webapp:v1 .
-docker push brax2507/delivery-tracker-webapp:v1
+docker build -t brax2507/delivery-tracker:v1 .
+docker push brax2507/delivery-tracker:v1
